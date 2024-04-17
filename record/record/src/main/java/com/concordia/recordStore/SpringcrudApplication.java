bbfdb46e9065d5425ef42c0013292fb566b82848
EricSadowski <118220751+EricSadowski@users.noreply.github.com>
2024-04-16 21:50:59 -0400

    Update Record

--- record/record/src/main/java/com/concordia/recordStore/SpringcrudApplication.java
+++ record/record/src/main/java/com/concordia/recordStore/SpringcrudApplication.java
@@ -2,8 +2,9 @@ package com.concordia.recordStore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class SpringcrudApplication {
 
 	public static void main(String[] args) {
